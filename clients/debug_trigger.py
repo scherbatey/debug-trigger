595a27994544057b8749eac73edc35511ebcd648
Oleksandr Shcherbatii <oleksandr.shcherbatii@blueyonder.com>
2021-03-22 19:51:59 +0100

    Update client code

--- clients/debug_trigger.py
+++ clients/debug_trigger.py
@@ -1,20 +1,34 @@
 import socket
-from sys import stderr
 import debugpy
+import sys
 
 
-def start_debug(id: str = "python", host="127.0.0.1", port=8989, debugpy_host="127.0.0.1", debugpy_port=4567):
-    debugpy.listen(debugpy_port)
+_this_process_attached = False
+
+
+def start_debug(id: str = "python", host="127.0.0.1", port=8989, debugpy_host="127.0.0.1", debugpy_port=4567, debugpy_port_autoincrement=True):
+    global _this_process_attached
+    if _this_process_attached:
+        return 0
+
+    while True:
+        try:
+            debugpy.listen(debugpy_port)
+            break
+        except RuntimeError:
+            if debugpy_port_autoincrement:
+                debugpy_port += 1
+            else:
+                raise
     try:
         sock = socket.create_connection((host, port))
         sock.send(bytes(f"{id}\r\n{debugpy_host}\r\n{debugpy_port}\r\n", "utf8"))
-        # print("Request sent!\nWaiting for responce...")
         ret_code = sock.recv(1)[0]
-        # print(f"Responce received! Code = {ret_code}")
         sock.close()
-        # print("Waiting for debugger to connect!")
-        debugpy.wait_for_client()
+        if ret_code == 0:
+            _this_process_attached = True
+            debugpy.wait_for_client()
         return ret_code
-    except:
-        # print(f"Cannot connect to {host}:{port}", file=stderr)
-        return -1 #response[0]
+    except Exception:
+        print(f"Cannot connect to {host}:{port}", file=sys.stderr)
+        return -1
