4c639cd6b702181b45a1eb24f3fc03b2339ef3bd
Oleksandr Shcherbatii <oleksandr.shcherbatii@blueyonder.com>
2021-03-22 18:13:37 +0100

    Update client code

--- clients/debug_trigger.py
+++ clients/debug_trigger.py
@@ -1,15 +1,20 @@
 import socket
-import os
+from sys import stderr
 import debugpy
 
 
-def start_debug(id: str = "python", host="127.0.0.1", port=8989, debugpy_port=5678):
-    sock = socket.create_connection((host, port))
-    if not sock:
-        return
+def start_debug(id: str = "python", host="127.0.0.1", port=8989, debugpy_host="127.0.0.1", debugpy_port=4567):
     debugpy.listen(debugpy_port)
-    sock.send(bytes(f"{id}\r\n\r\n{os.getpid()}\r\n", "utf8"))
-    ret_code = sock.recv(1)
-    sock.close()
-    debugpy.wait_for_client()
-    return ret_code
+    try:
+        sock = socket.create_connection((host, port))
+        sock.send(bytes(f"{id}\r\n{debugpy_host}\r\n{debugpy_port}\r\n", "utf8"))
+        # print("Request sent!\nWaiting for responce...")
+        ret_code = sock.recv(1)[0]
+        # print(f"Responce received! Code = {ret_code}")
+        sock.close()
+        # print("Waiting for debugger to connect!")
+        debugpy.wait_for_client()
+        return ret_code
+    except:
+        # print(f"Cannot connect to {host}:{port}", file=stderr)
+        return -1 #response[0]
