5b032a398a4fb08b93431e120a51c93fc36bef16
Oleksandr Shcherbatii <oleksandr.shcherbatii@blueyonder.com>
2021-03-31 15:18:29 +0200

    Update test app

--- test/test.c
+++ test/test.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    const int code = start_debug("test_c", 0, "127.0.0.1", 8989);
+    const int code = start_debug("cppdbg", 0, "127.0.0.1", 8989, 10000);
     printf("Code: %d\n", code);
     return code;
 }
